8481d46c299af65ae40eca6a1a586c87c65daea7
RevenantParagon <brunocosta176186@gmail.com>
2019-10-09 22:29:28 -0300

    Otsu

---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 416);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ler";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(24, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(484, 370);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(541, 28);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.IsVisibleInLegend = false;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(442, 370);
+            this.chart1.TabIndex = 235;
+            this.chart1.Text = "chart1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(105, 416);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(135, 23);
+            this.button3.TabIndex = 236;
+            this.button3.Text = "Equalizar Histograma";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(258, 416);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 237;
+            this.button2.Text = "Otsu";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(433, 416);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 238;
+            this.button4.Text = "Imagem";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 468);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DIPLi;
+using nQuant;
+
+
+
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+
+        Imagem I;
+
+        static Imagem O = new Imagem(256, 256, TipoImagem.Colorida);
+
+        Imagem R = O.Separar(PlanoImagem.Red);
+        Imagem G = O.Separar(PlanoImagem.Green);
+        Imagem B = O.Separar(PlanoImagem.Blue);
+
+
+
+        public void MostrarImagem(DIPLi.Imagem IMG)
+        {
+          
+            pictureBox1.Image = IMG.ToBitmap();
+        }
+       
+
+        private double[] calcularHistograma(DIPLi.Imagem IMG)
+        {
+            double[] valores = new double[256];
+            int niveldecinza;
+            int i;
+            for (i = 0; i < IMG.Altura; i++)
+            {
+                for (int j = 0; j < IMG.Largura; j++)
+                {
+                    niveldecinza = (int)IMG[i, j];
+                    valores[niveldecinza] = valores[niveldecinza] + 1;
+
+                }
+            }
+
+
+            return valores;
+
+        }
+        public int Otsu(Imagem I)
+        {
+
+            double[] histData = new double[256];
+            histData = calcularHistograma(I);
+
+            // Total number of pixels
+            int total = I.Altura * I.Largura;
+
+            double sum = 0;
+            for (int t = 0; t < 256; t++) sum += t * histData[t];
+
+            double sumB = 0;
+            int wB = 0;
+            int wF = 0;
+
+            double varMax = 0;
+            int threshold = 0;
+
+            for (int t = 0; t < 256; t++)
+            {
+                wB += (int)histData[t];               // Weight Background
+                if (wB == 0) continue;
+
+                wF = total - wB;                 // Weight Foreground
+                if (wF == 0) break;
+
+                sumB += (double)(t * histData[t]);
+
+                double mB = sumB / wB;            // Mean Background
+                double mF = (sum - sumB) / wF;    // Mean Foreground
+
+                // Calculate Between Class Variance
+                double varBetween = (double)wB * (double)wF * (mB - mF) * (mB - mF);
+
+                // Check if new maximum found
+                if (varBetween > varMax)
+                {
+                    varMax = varBetween;
+                    threshold = t;
+                }
+            }
+            return threshold;
+        }
+
+
+
+public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                   
+
+            I = I.ToGrayscale();
+            MostrarImagem(I);
+
+            double[] yValues = new double[256];
+            yValues = calcularHistograma(I);
+
+            string[] xValues = new string[256];
+
+            for (int i = 0; i < 256; i++)
+            {
+                xValues[i] = i.ToString();
+            }
+
+            chart1.ChartAreas[0].AxisX.Maximum = 256;
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            chart1.Series["Series1"].Points.DataBindXY(xValues, yValues);
+
+
+
+            //Imagem I = new Imagem(256, 256);
+
+            //for (int i = 0; i < I.Altura; i++)
+            //    for (int j = 0; j < I.Largura; j++)
+            //        I[i, j] = (i * j) % 256;
+
+            //I.Salvar("ex1.png");
+
+
+
+            //            Imagem O = new Imagem(256, 256, TipoImagem.Colorida);
+
+            //for (int i = 0; i < I.Altura; i++)
+            //    for (int j = 0; j < I.Largura; j++)
+            //    {
+            //        O[i, j, 0] = (i * j) % 256;
+            //        O[i, j, 1] = (i + j) % 256;
+            //        O[i, j, 2] = (i - j) % 256;
+            //    }
+
+            //O.Salvar("ex2.jpg");
+            //O.ToGrayscale().Salvar("ex3.jpg");
+
+            //Imagem R = O.Separar(PlanoImagem.Red);
+            //Imagem G = O.Separar(PlanoImagem.Green);
+            //Imagem B = O.Separar(PlanoImagem.Blue);
+
+            //for (int i = 0; i < I.Altura; i++)
+            //    for (int j = 0; j < I.Largura; j++)
+            //    {
+            //        if (R[i, j] < 127)
+            //            R[i, j] = 0;
+            //        else
+            //            R[i, j] = 255;
+
+            //        if (G[i, j] < 127)
+            //            G[i, j] = 0;
+
+            //        if (B[i, j] < 100)
+            //            B[i, j] *= 0.8;
+            //        else if (B[i, j] < 200)
+            //            B[i, j] *= 1.2;
+            //        else
+            //            B[i, j] *= 0.8;
+
+            //        B[i, j] %= 256;
+            //    }
+
+            //Imagem.Combinar(R, G, B).Salvar("ex4.bmp");
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           
+            double[] hist = new double[256];
+            hist = calcularHistograma(I);
+
+            for (int i = 0; i < 256; i++)
+            {
+                hist[i] = hist[i] / (I.Largura * I.Altura);
+
+            }
+
+            for (int i = 1; i < 256; i++)
+            {
+                hist[i] = hist[i] + hist[i - 1];
+
+            }
+            for (int i = 0; i < 256; i++)
+            {
+                hist[i] = hist[i] * 255;
+
+
+            }
+
+
+
+            for (int i = 0; i < I.Altura; i++)
+            {
+                for (int j = 0; j < I.Largura; j++)
+                {
+                    double valorantes = I[i, j];
+                    double valordepois = hist[(int)valorantes];
+                    I[i, j] = valordepois;
+                }
+            }
+
+            double[] yValues = new double[256];
+            yValues = calcularHistograma(I);
+
+            string[] xValues = new string[256];
+
+            for (int i = 0; i < 256; i++)
+            {
+                xValues[i] = i.ToString();
+            }
+
+            chart1.ChartAreas[0].AxisX.Maximum = 256;
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            chart1.Series["Series1"].Points.DataBindXY(xValues, yValues);
+
+            MostrarImagem(I);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            I = I.ToGrayscale();
+
+            int limiar = Otsu(I);
+            
+            MessageBox.Show(limiar.ToString());
+
+            for (int i = 0; i < I.Altura; i++)
+            {
+                for (int j = 0; j < I.Largura; j++)
+                {
+                    if (I[i, j] <= limiar)
+                    {
+                        I[i, j] = 0;
+                    }
+                    else
+                    {
+                        I[i, j] = 255;
+                    }
+                    
+                }
+            }
+
+            MostrarImagem(I);
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Multiselect = false;
+            openFileDialog1.Title = "Selecionar Imagem";
+            openFileDialog1.InitialDirectory = "C:\\";
+            openFileDialog1.Filter = "Images (*.BMP;*.JPG;*.GIF,*.PNG,*.TIFF)|*.BMP;*.JPG;*.GIF;*.PNG;*.TIFF|" + "All files (*.*)|*.*";
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.ReadOnlyChecked = true;
+            openFileDialog1.ShowReadOnly = true;
+            openFileDialog1.FileName = "";
+
+            DialogResult dr = openFileDialog1.ShowDialog();
+
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                string file = openFileDialog1.FileName.ToString();
+                I = new Imagem(file);
+            }
+        }
+    }
+}
