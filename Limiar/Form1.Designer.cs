9a568e3502d6b1cdbc211667c843a52980667fdf
RevenantParagon <brunocosta176186@gmail.com>
2019-10-09 22:28:30 -0300

    Commit

---
+++ Limiar/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Limiar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(318, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 318);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Abrir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(318, 318);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Limiar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Limiar/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DIPLi;
+
+namespace Limiar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Imagem imagem;
+
+        public bool Abrir()
+        {
+            openFileDialog1.Multiselect = false;
+            openFileDialog1.Title = "Selecionar Imagem";
+            openFileDialog1.InitialDirectory = "C:\\";
+            openFileDialog1.Filter = "Images (*.BMP;*.JPG;*.GIF,*.PNG,*.TIFF)|*.BMP;*.JPG;*.GIF;*.PNG;*.TIFF|" + "All files (*.*)|*.*";
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.ReadOnlyChecked = true;
+            openFileDialog1.ShowReadOnly = true;
+            openFileDialog1.FileName = "";
+
+            DialogResult dr = openFileDialog1.ShowDialog();
+
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                string file = openFileDialog1.FileName.ToString();
+                imagem = new Imagem(file);
+                return true;
+            }
+            else
+                return false;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (Abrir())
+            {
+                pictureBox1.Image = imagem.ToBitmap();
+                imagem = imagem.ToGrayscale();
+            }
+        }
+
+        public Imagem PassaBaixa(int m)
+        {
+            int[,] vs = new int[m, m];
+            for (int a = 0; a < m; a++)
+            {
+                for (int b = 0; b < m; b++)
+                {
+                    vs.SetValue(1, a, b);
+                }
+            }
+
+            Imagem R = new Imagem(imagem.Largura, imagem.Altura, TipoImagem.Monocromatica);
+            double soma;
+
+            for (int i = ((m - 1) / 2); i < imagem.Altura - ((m - 1) / 2); i++)
+            {
+                for (int j = ((m - 1) / 2); j < imagem.Largura - ((m - 1) / 2); j++)
+                {
+                    soma = 0;
+                    for (int a = -((m - 1) / 2); a < ((m + 1) / 2); a++)
+                    {
+                        for (int b = -((m - 1) / 2); b < ((m + 1) / 2); b++)
+                        {
+                            soma = soma + (imagem[i + a, j + b] * vs[a + ((m - 1) / 2), b + ((m - 1) / 2)]);
+                        }
+                    }
+                    soma = soma / (m * m);
+                    R[i, j] = soma;
+                }
+            }
+            return R;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Imagem passa = PassaBaixa(11);
+
+            for (int i = 0; i < passa.Altura; i++)
+            {
+                for (int j = 0; j < passa.Largura; j++)
+                {
+                    if (passa[i, j] <= 206)
+                    {
+                        passa[i, j] = 0;
+                    }
+                    else
+                    {
+                        passa[i, j] = 255;
+                    }
+                }
+            }
+            pictureBox2.Image = passa.ToBitmap();
+        }
+    }
+}
